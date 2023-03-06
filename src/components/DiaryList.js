6fb8a8b55c5b36ad9d1e9df73b00c9dfc1f42357
hskimstudy <hskimstudy@gmail.com>
2023-03-06 15:52:03 +0900

    +imgWrapperStyle
    +DiaryItem.emotion_img_wrapper
    +더미데이터 시간 바꿔서 정렬함

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import MyButton from './MyButton';
+import DiaryItem from './DiaryItem';
 
 const sortOptiononList = [
   { value: 'latest', name: "최신순" },
@@ -84,9 +85,7 @@ const DiaryList = ({ diaryList }) => {
 
 
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>
-          {it.content}{it.emotion}
-        </div>
+        <DiaryItem key={it.id}{...it} />
       ))}
     </div>
   )
